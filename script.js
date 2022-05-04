cb38bb213ad0e3618923114eb22b3bedf2cc4cc6
AUDRIUS BEJERIS <audriusbejeris@gmail.com>
2022-05-04 16:33:26 +0100

    pataisymas

--- script.js
+++ script.js
@@ -15,12 +15,22 @@ var characterNames = ["John Snow", "Cersei Lannister", "Daenerys Targaryen", "Th
 console.log(characterNames[0] + " and " + characterNames[characterNames.length - 1])
 
 var characterName = prompt ("įveskite skaičių nuo 1 iki 6")
+
+while(isNaN(characterName) == true) {
+var characterName = prompt ("Įveskite tik skaičių")
+}
+
+while(characterName > 6 || characterName < 1) {
+    var characterName = prompt("Tai nėra skaičius tarp 1 ir 6")
+}
 console.log(characterNames[characterName -1])
 
 
+// nelabai iseina padaryt, kad veiktu abu loopai, nes po neteisingo skaiciaus ivedus raide nebepraso pataisyt..
 
 // lyg ir veikia taip, bet sakyciau reiktu daryt kazka su for(var) jei sarasas butu zymiai ilgesnis nei 6 vardai, tik neismastau kaip kolkas. 
 
 // for(var i=0; i < characterNames.length; i++) {
     
 // }
+
